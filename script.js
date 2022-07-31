7fe6cd89c8a8a754df09555b62084f193ae1b56f
Sam George Mathew <sam.mathew19113@gmail.com>
2022-07-31 19:42:59 +0530

    get choice of weapon from the player and computer

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 let playerScore = 0;
 let computerScore = 0;
+let getPlayerChoice;
 
-const myTools = ["rock","paper","scissors"];
-// Function returns random computer choice 
+const myTools = ["rock", "paper", "scissors"];
+// Function returns random computer choice
 
 function getComputerChoice() {
-    return myTools[Math.floor(Math.random() * myTools.length)];
-};
+  return myTools[Math.floor(Math.random() * myTools.length)];
+}
 
 // Function that plays one round of the game
 
 function playRound(playerSelection, computerSelection) {
-    if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')
-    ) {
-        return "You won, " + playerSelection + " beats " + computerSelection + ".";
-    } else if (playerSelection === computerSelection) {
-        return "It\'s a draw.";
-    } else if ( (computerSelection === 'rock' && playerSelection === 'scissors')||
-                (computerSelection === 'paper' && playerSelection === 'rock')||
-                (computerSelection === 'scissors' && playerSelection == 'paper') ) {
-        return "You lose, " + computerSelection + " beats " + playerSelection + ".";
-    } else {
-        return "There is a disturbance in the Force.";
-    }
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return "You won, " + playerSelection + " beats " + computerSelection + ".";
+  } else if (playerSelection === computerSelection) {
+    return "It's a draw.";
+  } else if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "paper" && playerSelection === "rock") ||
+    (computerSelection === "scissors" && playerSelection == "paper")
+  ) {
+    return "You lose, " + computerSelection + " beats " + playerSelection + ".";
+  } else {
+    return "There is a disturbance in the Force.";
+  }
+}
 
-};
+// get choices of player and computer
+
+
+let playerSelection;
+  if (
+    playerSelection != "rock" ||
+    playerSelection != "paper" ||
+    playerSelection != "scissors"
+  ) {
+    playerSelection = prompt("Choose from rock, paper or scissors.").toLowerCase();
+  };
+
+
+const computerSelection = getComputerChoice();
