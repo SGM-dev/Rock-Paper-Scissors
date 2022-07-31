1b16e76216ab16d9acae4b5ca2237ebf7f79b04f
Sam George Mathew <sam.mathew19113@gmail.com>
2022-07-31 19:14:03 +0530

    create a function to play 1 round

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ function getComputerChoice() {
     return myTools[Math.floor(Math.random() * myTools.length)];
 };
 
+// Function that plays one round of the game
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        return "You won, " + playerSelection + " beats " + computerSelection + ".";
+    } else if (playerSelection === computerSelection) {
+        return "It\'s a draw.";
+    } else if ( (computerSelection === 'rock' && playerSelection === 'scissors')||
+                (computerSelection === 'paper' && playerSelection === 'rock')||
+                (computerSelection === 'scissors' && playerSelection == 'paper') ) {
+        return "You lose, " + computerSelection + " beats " + playerSelection + ".";
+    } else {
+        return "There is a disturbance in the Force.";
+    }
+
+};
