576b74d1bd9fd11465797a656d67485a4dbb67ec
Sam George Mathew <sam.mathew19113@gmail.com>
2022-08-07 10:32:42 +0530

    add event listeners to get player's choice

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const optionBtn = document.querySelectorAll('div.optionBtn button');
-const roundResults = document.querySelector('#roundResults');
-const playerPoints = document.querySelector('#playerScore');
-const computerPoints = document.querySelector('#computerScore');
-const resetBtn = document.querySelector('#reset');
-
+const optionBtn = document.querySelectorAll("div.optionBtn button");
+const roundResults = document.querySelector("#roundResults");
+const playerPoints = document.querySelector("#playerScore");
+const computerPoints = document.querySelector("#computerScore");
+const resetBtn = document.querySelector("#reset");
 
 //refresh page for new game
-resetBtn.addEventListener('click', () => location.reload());
+resetBtn.addEventListener("click", () => location.reload());
+
+//get player's choice
+optionBtn.forEach((button) => {
+  button.addEventListener("click", getPlayerChoice);
+});
